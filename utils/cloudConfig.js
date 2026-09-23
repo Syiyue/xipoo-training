6b70dd6091c856698c211803ccd0ef4643481c50
Syiyue <13703810175@163.com>
2026-09-23 19:33:13 +0800

    细节修改

--- utils/cloudConfig.js
+++ utils/cloudConfig.js
@@ -1,7 +1,7 @@
 // 小程序云函数与数据库在 cloudbase-d7…；OCR 云托管在独立 prod 环境。
 // 云函数通过 CONTAINER_BASE 访问 prod 的 flask-tbst，不要把 wx.cloud 强切到
 // prod-d3…：该环境当前没有可用的云函数元数据，调用 schedule 会失败。
-const CLOUD_ENV_ID = 'cloudbase-d7gowjn4mf367be4d'
+const CLOUD_ENV_ID = 'cloudbase-d9g6yi49t3d0db634'
 
 /**
  * 检查云开发环境是否可用
